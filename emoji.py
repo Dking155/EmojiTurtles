67db924a9b8eaa47b366644ca857e9c3112103ba
100134969 <kingpatrick455@gmail.com>
2019-09-11 09:02:51 -0500

    Initial commit

---
+++ emoji.py
@@ -0,0 +1,37 @@
+import turtle
+turtle.bgcolor("black")
+red=turtle
+red.color("red")
+red.circle(180)
+import turtle
+blue=turtle
+blue.color("blue")
+blue.left(90)
+blue.penup()
+blue.forward(240)
+blue.left(90)
+blue.forward(90)
+blue.pendown()
+blue.circle(35)
+import turtle
+green=turtle
+green.color("green")
+green.left(180)
+green.penup()
+green.forward(180)
+green.pendown()
+green.right(180)
+green.circle(35)
+import turtle
+yellow=turtle
+yellow.color("yellow")
+yellow.penup()
+yellow.forward(95)
+yellow.left(90)
+yellow.forward(160)
+yellow.right(90)
+yellow.pendown()
+yellow.forward(80)
+yellow.left(180)
+yellow.forward(190)
+yellow.exitonclick()
